17a78d6166f051354d6a33e8da6e21654e61e421
Sociosarbis <136657577@qq.com>
2021-02-05 00:38:19 +0800

    Updates

---
+++ precache-manifest.96ce4767dc0e4554044809a4849204bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25eac0c48918562ccbc3e367f7733ad",
+    "url": "/tempExhibit/index.html"
+  },
+  {
+    "revision": "4aec05d38b276b0b1336",
+    "url": "/tempExhibit/static/css/main.0b5a0dec.chunk.css"
+  },
+  {
+    "revision": "334e580897939eafc96a",
+    "url": "/tempExhibit/static/js/2.52094878.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/tempExhibit/static/js/2.52094878.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4aec05d38b276b0b1336",
+    "url": "/tempExhibit/static/js/main.407e1366.chunk.js"
+  },
+  {
+    "revision": "cf0dbacb30717b7b20a8",
+    "url": "/tempExhibit/static/js/runtime-main.b052259b.js"
+  }
+]);
