86a3ebac22c80e50a0d95bcb9cf51cb9d3438c9d
Sociosarbis <136657577@qq.com>
2021-02-08 23:38:29 +0800

    ci: :hammer: comment proxy

--- functions/helpers/axios.ts
+++ functions/helpers/axios.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
-export default axios.create(
-  process.env.NETLIFY
-    ? {}
-    : {
-        proxy: {
-          host: '127.0.0.1',
-          port: 10809,
-        },
-      },
-);
+export default axios.create(/*{
+  proxy: {
+    host: '127.0.0.1',
+    port: 10809,
+  },
+}*/);
