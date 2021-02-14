8ec72c594de3d4b08c2c03d15702f5c740576a44
Sociosarbis <136657577@qq.com>
2021-02-14 11:48:36 +0800

    feat: :sparkles: 根据路由改变title

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cls from 'classnames';
-import { Route, useHistory, useLocation } from 'react-router-dom';
+import { Route, useHistory, useLocation, matchPath } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 import { CustomThemeProvider } from './CustomThemeProvider';
 import { useLoading } from './components/Loading';
@@ -27,37 +27,84 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+type RouteConfig = {
+  path: string;
+  component: () => JSX.Element;
+  meta: {
+    title: string;
+  };
+};
+
+const routeConfig = [
+  {
+    path: '/',
+    component: Home,
+    meta: {
+      title: '播放',
+    },
+  },
+  {
+    path: '/history',
+    component: History,
+    meta: {
+      title: '历史',
+    },
+  },
+  {
+    path: '/calendar',
+    component: Calendar,
+    meta: {
+      title: '每日放送',
+    },
+  },
+];
+
+function Title() {
+  const location = useLocation<any>();
+  const matches: RouteConfig[] = [];
+  for (const route of routeConfig) {
+    if (
+      matchPath(location.pathname, {
+        path: route.path,
+      })
+    ) {
+      matches.push(route);
+    }
+  }
+
+  if (matches.length) {
+    for (const match of matches.reverse()) {
+      if (match.meta.title) {
+        document.title = match.meta.title;
+        break;
+      }
+    }
+  }
+
+  return null;
+}
+
 function MainApp() {
   const player = usePlayer();
   return (
     <PlayerContext.Provider value={player}>
-      <Route component={Home}></Route>
-      <Route path="/history">
-        {({ match }) => (
-          <CSSTransition
-            in={match != null}
-            classNames="slide-up"
-            timeout={{ enter: 250, exit: 200 }}
-            mountOnEnter={true}
-            unmountOnExit={true}
-          >
-            <History />
-          </CSSTransition>
-        )}
-      </Route>
-      <Route path="/calendar">
-        {({ match }) => (
-          <CSSTransition
-            in={match != null}
-            classNames="slide-up"
-            timeout={{ enter: 250, exit: 200 }}
-            mountOnEnter={true}
-            unmountOnExit={true}
-          >
-            <Calendar />
-          </CSSTransition>
-        )}
-      </Route>
+      {routeConfig.map((item) => {
+        return (
+          <Route path={item.path} key={item.path}>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                classNames="slide-up"
+                timeout={{ enter: 250, exit: 200 }}
+                mountOnEnter={true}
+                unmountOnExit={true}
+              >
+                <item.component />
+              </CSSTransition>
+            )}
+          </Route>
+        );
+      })}
     </PlayerContext.Provider>
   );
 }
@@ -83,6 +130,7 @@ function App() {
   );
   return (
     <CustomThemeProvider>
+      <Title />
       <GlobalContext.Provider value={{ showMessage, withLoading, setLoading }}>
         <MainApp />
       </GlobalContext.Provider>
@@ -90,22 +138,18 @@ function App() {
         showLabels
         classes={{ root: cls(baseClasses.btn, classes.bottom, 'w-full') }}
       >
-        {[
-          ['播放', '/'],
-          ['历史', '/history'],
-          ['每日放送', '/calendar'],
-        ].map((item, i) => {
+        {routeConfig.map((item, i) => {
           return (
             <BottomNavigationAction
               key={i}
               classes={{
                 root: cls(
                   classes.bottomItem,
-                  location.pathname === item[1] ? baseClasses.container : '',
+                  location.pathname === item.path ? baseClasses.container : '',
                 ),
               }}
-              label={<Typography variant="h6">{item[0]}</Typography>}
-              onClick={() => history.replace(item[1])}
+              label={<Typography variant="h6">{item.meta.title}</Typography>}
+              onClick={() => history.replace(item.path)}
             ></BottomNavigationAction>
           );
         })}
