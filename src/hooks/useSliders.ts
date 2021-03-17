d53beabf4dda63bb27866052f3c74e8521280544
Sociosarbis <136657577@qq.com>
2021-03-18 00:29:17 +0800

    fix: :bug: count slice

--- src/hooks/useSliders.ts
+++ src/hooks/useSliders.ts
@@ -9,13 +9,17 @@ type Slider = {
 export default function useSlider(itemLength: number, pageSize: number) {
   const initialSliders = useMemo(() => {
     const sliders = [];
+    const count = itemLength % 25;
     itemLength = Math.floor(itemLength / pageSize);
+    if (count == 0 && itemLength !== 0) {
+      itemLength--;
+    }
     let base = 1;
     while (itemLength !== 0) {
       const count = itemLength % 10;
       itemLength = (itemLength - count) / 10;
       if (count === 0 && itemLength !== 0) {
-        sliders.push({ base: base * pageSize, max: 9 * base * pageSize });
+        sliders.push({ base: base * pageSize, max: 10 * base * pageSize });
         itemLength -= 1;
       } else {
         sliders.push({ base: base * pageSize, max: count * base * pageSize });
